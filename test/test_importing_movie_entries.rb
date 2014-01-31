783a8bac396d6d23b7432ae1faaf86eed3614240
Lori McCurry <lori.mccurry@gmail.com>
2014-01-31 14:42:25 -0600

    Add sample data and importing movies tests

---
+++ test/test_importing_movie_entries.rb
@@ -0,0 +1,47 @@
+require_relative 'helper'
+require_relative '../lib/importer'
+
+class TestImportingMovieEntries < MovieTest
+  def import_data
+    Importer.import("test/sample_movie_entry_data.csv")
+  end
+
+  def test_the_correct_number_of_movie_entries_are_imported
+    import_data
+    assert_equal 5, MovieEntries.all.count
+  end
+
+  def test_movie_entries_are_imported_fully
+    import_data
+    expected = [
+      "Fight Club, f, f, t, f, none",
+      "Magic Mike, t, f, f, f, 10",
+      "The Social Network, t, t, f, f, 85",
+      "Up, t, f, f, t, 95",
+      "When Harry Met Sally, t, t, f, f, 90",
+    ]
+    actual = MovieEntries.all.map do |movie_entry|
+      "#{movie_entry.title}, #{movie_entry.seen}, #{movie_entry.own}, #{movie_entry.wishlist_see}, #{movie_entry.wishlist_own}, #{movie_entry.user_rating}"
+    end
+    assert_equal expected, actual
+  end
+
+  def test_extra_movies_arent_created
+    import_data
+    assert 5, Movie.all.count
+  end
+
+  def test_movies_are_created_as_needed
+    Movie.find_or_create("Good Will Hunting")
+    Movie.find_or_create("Up")
+    import_data
+    assert 7, Movie.all.count
+  end
+
+  def test_data_isnt_duplicated
+    import_data
+    expected = ["Fight Club", "Magic Mike", "The Social Network", "Up", "When Harry Met Sally"]
+    assert_equal expected, Movie.all.map(&:title)
+  end
+
+end
